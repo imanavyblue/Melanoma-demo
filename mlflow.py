29403cc60690cfc2c80ca2cb7196f750572f4662
imanavyblue <134798126+imanavyblue@users.noreply.github.com>
2024-08-27 15:32:15 +0700

    Create mlflow.py

---
+++ mlflow.py
@@ -0,0 +1,10 @@
+import mlflow
+import mlflow.keras
+
+def log_model_and_metrics(model, loss, accuracy):
+    mlflow.start_run()
+    mlflow.keras.log_model(model, "model")
+    mlflow.log_param("learning_rate", 0.0001)
+    mlflow.log_metric("test_loss", loss)
+    mlflow.log_metric("test_accuracy", accuracy)
+    mlflow.end_run()
