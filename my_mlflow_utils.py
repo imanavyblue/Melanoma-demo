a9658c9d73c8127e81a87a6d079ef3139048d523
imanavyblue <134798126+imanavyblue@users.noreply.github.com>
2024-08-28 17:57:39 +0700

    Update my_mlflow_utils.py

--- my_mlflow_utils.py
+++ my_mlflow_utils.py
@@ -1,9 +1,11 @@
 import mlflow
 import mlflow.keras
 
-def log_model_and_metrics(model, loss, accuracy):
-    with mlflow.start_run():
-        mlflow.keras.log_model(model, "model")
-        mlflow.log_param("learning_rate", 0.0001)
-        mlflow.log_metric("test_loss", loss)
-        mlflow.log_metric("test_accuracy", accuracy)
+def log_model_and_metrics(model, loss, accuracy, recall):
+    mlflow.start_run()
+    mlflow.keras.log_model(model, "model")
+    mlflow.log_param("learning_rate", 0.0001)  # ค่า learning_rate ที่ใช้
+    mlflow.log_metric("test_loss", loss)
+    mlflow.log_metric("test_accuracy", accuracy)
+    mlflow.log_metric("test_recall", recall)  # บันทึกค่า recall
+    mlflow.end_run()
