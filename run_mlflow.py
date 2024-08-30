b20133bef8b0a3b40d75b48586b5ada52d159631
imanavyblue <134798126+imanavyblue@users.noreply.github.com>
2024-08-30 15:45:09 +0700

    Update run_mlflow.py

--- run_mlflow.py
+++ run_mlflow.py
@@ -4,9 +4,44 @@ from keras.applications import InceptionV3
 from keras.models import Model
 from keras.layers import Input, Dense, GlobalAveragePooling2D
 import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def download_folder_from_google_drive(folder_url, destination):
     gdown.download(folder_url, destination, quiet=False, fuzzy=True)
+
+def load_data(train_dir, val_dir):
+    # Data augmentation สำหรับการฝึกอบรม
+    train_datagen = ImageDataGenerator(
+        rescale=1./255,
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest'
+    )
+
+    # Data augmentation สำหรับ validation (แค่ rescale)
+    val_datagen = ImageDataGenerator(rescale=1./255)
+
+    # เตรียมข้อมูลการฝึกอบรม
+    train_data = train_datagen.flow_from_directory(
+        train_dir,
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical'
+    )
+
+    # เตรียมข้อมูล validation
+    val_data = val_datagen.flow_from_directory(
+        val_dir,
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical'
+    )
+
+    return train_data, val_data
 
 def create_model():
     input_shape = (224, 224, 3)
@@ -42,19 +77,18 @@ def train_and_log_model(model, train_data, val_data):
 
 def main():
     # URL ของโฟลเดอร์จาก Google Drive
-    folder_url = 'https://drive.google.com/drive/folders/1KilXUDLq23K8ttEw33fjzikq0B6E4R57?usp=sharing'
+    folder_url = 'https://drive.google.com/drive/folders/1j_hlGK9tKr88h9W_Du2X6p8yKhk8fepb?usp=sharing'
     destination = 'data/'
 
     # สร้างโฟลเดอร์ปลายทางถ้ายังไม่มี
     os.makedirs(destination, exist_ok=True)
 
-
     # ดาวน์โหลดข้อมูลจาก Google Drive
     download_folder_from_google_drive(folder_url, destination)
 
     # กำหนดไดเรกทอรีข้อมูลการฝึกอบรมและ validation
-    train_dir = os.path.join(destination, 'train_data')
-    val_dir = os.path.join(destination, 'validation_data')
+    train_dir = os.path.join(destination, 'train')
+    val_dir = os.path.join(destination, 'val')
 
     # โหลดข้อมูล
     train_data, val_data = load_data(train_dir, val_dir)
